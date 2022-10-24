f44fff549e63d139b6d3333680d1505f57e8d4e2
Olha Chornei <pradocw8053@gmail.com>
2022-10-24 17:19:00 +0300

    Update Profile.styled.jsx

--- src/components/Profile/Profile.styled.jsx
+++ src/components/Profile/Profile.styled.jsx
@@ -1,51 +1,43 @@
 import styled from 'styled-components';
 
-export const Profile = styled.div
-`  width: 250px;
+export const UseCard = styled.div`  width: 250px;
   text-align: center;
   margin: auto;
   border: solid 1px #d8e0ea;`
 
-export const Description = styled.div
-`  width: 250px;
+export const Description = styled.div`  width: 250px;
   height: auto;`
 
-export const Avatar = styled.img
-`  width: 128px;
+export const Avatar = styled.img`  width: 128px;
   height: 128px;
   border-radius: 50%;
   overflow: hidden;
   text-align: center;
   margin: auto;`
 
-export const Name = styled.p
-`  font-size: 18px;
+export const Name = styled.p`  font-size: 18px;
   color: #333333;
   font-weight: 700;
   margin-top: 30px;`
 
-export const Tag = styled.p
-`  font-size: 14px;
+export const Tag = styled.p`  font-size: 14px;
   color: #666666;
   text-align: center;
   margin-top: 10px;`
 
-export const Location = styled.p
-`  font-size: 14px;
+export const Location = styled.p`  font-size: 14px;
   color: #97a2ae;
   line-height: 32px;
   margin-top: 10px;
   margin-bottom: 30px;`
 
-export const StatsList = styled.ul
-`  display: flex;
+export const StatsList = styled.ul`  display: flex;
   justify-content: space-evenly;
   list-style: none;
   background-color: #f3f6f9;
   border-radius: 0 0 5px 5px;`
 
-export const StatsItem = styled.li
-`  flex-direction: column;
+export const StatsItem = styled.li`  flex-direction: column;
   align-items: center;
   border: solid 1px #d8e0ea;
   width: 85px;`
