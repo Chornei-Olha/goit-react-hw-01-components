5e75fc5e977aa7302cc7941736efd8aeeb34ab62
Olha Chornei <pradocw8053@gmail.com>
2022-10-24 17:25:34 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,20 +12,28 @@ import user from '../data/user.json';
 // import { TransactionHistory} from './TransactionHistory/TransactionHistory';
 // import transactions from '../data/transactions.json'
 
+// export const App = () => {
+//   return (
+//     <div>
+//       <Profile
+//         username={user.username}
+//         tag={user.tag}
+//         location={user.location}
+//         avatar={user.avatar}
+//         stats={user.stats}
+//       />
+//       {/* <Statistics title="Upload stats" stats={data} />
+//       <Statistics stats={data} />
+//       <FriendList friends={friends} />;
+//       <TransactionHistory items={transactions} />; */}
+//     </div>
+//   );
+// };
+
+
+
 export const App = () => {
   return (
-    <div>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      {/* <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
-      <FriendList friends={friends} />;
-      <TransactionHistory items={transactions} />; */}
-    </div>
+    <h2> wokr</h2>
   );
 };
