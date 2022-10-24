ce9a5bf31d9a13dec82c0a1c18988933aa12573f
Olha Chornei <pradocw8053@gmail.com>
2022-10-24 13:43:51 +0300

    add commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import { Profile } from './Profile/Profile';
 import user from '../data/user.json';
 
-import { Statistics } from './Statistics/Statistics';
-import data from '../data/data.json';
+// import { Statistics } from './Statistics/Statistics';
+// import data from '../data/data.json';
 
-import { FriendList } from './FriendList/FriendList';
-import friends from '../data/friends.json';
+// import { FriendList } from './FriendList/FriendList';
+// import friends from '../data/friends.json';
 
-import { TransactionHistory} from './TransactionHistory/TransactionHistory';
-import transactions from '../data/transactions.json'
+// import { TransactionHistory} from './TransactionHistory/TransactionHistory';
+// import transactions from '../data/transactions.json'
 
 export const App = () => {
   return (
@@ -20,10 +20,10 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />;
-      <TransactionHistory items={transactions} />;
+      <TransactionHistory items={transactions} />; */}
     </div>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 import {
   UserCard,
@@ -36,14 +36,14 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
     </UserCard>
 }
 
-Profile.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  stats: PropTypes.exact({
-    followers: PropTypes.number.isRequired,
-    views: PropTypes.number.isRequired,
-    likes: PropTypes.number.isRequired,
-  }),
-};
+// Profile.propTypes = {
+//   avatar: PropTypes.string.isRequired,
+//   name: PropTypes.string.isRequired,
+//   tag: PropTypes.string.isRequired,
+//   location: PropTypes.string.isRequired,
+//   stats: PropTypes.exact({
+//     followers: PropTypes.number.isRequired,
+//     views: PropTypes.number.isRequired,
+//     likes: PropTypes.number.isRequired,
+//   }),
+// };
